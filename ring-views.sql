20311cdcfde43a0fea0a8bbfa418b7035d45e5ba
Vinícius Hoyer <vhoyer@live.com>
2016-10-10 20:58:34 -0300

    Update ring-views.sql

--- ring-views.sql
+++ ring-views.sql
@@ -1,1 +1,4 @@
-create view `gp_adms` u.name as 'Nome do user', g.name as 'Nome do grupo', if(r.is_adm = 1, 'sim', 'nao') as 'E administrador?' from rel_groups r left join groups g on r.id_groups = g.id left join users u on u.email = r.id_users;
+/*View dos integrantes do grupo, retorna todos os usuarios do grupo e se são admnistradores ou não*/
+create view `gp_adms` as 
+select u.name as 'Nome do user', g.name as 'Nome do grupo', if(r.is_adm = 1, 's', 'n') as 'E administrador?' 
+from rel_groups r left join groups g on r.id_groups = g.id left join users u on u.email = r.id_users;
