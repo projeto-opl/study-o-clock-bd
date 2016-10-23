ad92a7d0b6269b6c26a1de857d7d74193637cf1a
vhoyer <vhoyer@live.com>
2016-10-23 12:06:59 -0200

    Delete everything on table before inserting things
    so there is no error of duplicated primary key
    
    Signed-off-by: vhoyer <vhoyer@live.com>

--- rinf-inserts.sql
+++ rinf-inserts.sql
@@ -1,15 +1,18 @@
 use rinf;
 
+delete from cities;
 insert into cities(`name`) values
 ('São josé dos campos'),
 ('Jacareí'),
 ('Taubaté');
 
+delete from schools;
 insert into schools(`name`) values
 ('ETEP Esplanada'),
 ('ETEP Jacareí'),
 ('ETEP Taubaté');
 
+delete from users;
 insert into users(email, `name`, pass, id_schools, sex, id_cities, birthdate) values
 ('vhoyer@live.com', 'Vinicius Hoyer', 'pass123', 1, 'm', 1, '1999-12-06'),
 ('test2@email.com', 'Joaquinvaldison', 'pass123', 1, 'm', 1, '2016-04-15'),
@@ -17,6 +20,7 @@ insert into users(email, `name`, pass, id_schools, sex, id_cities, birthdate) va
 ('test4@email.com', 'Jhon Titor', 'pass123', 1, 'm', 1, '2000-06-30'),
 ('test5@email.com', 'joan filterston', 'pass123', 1, 'f', 1, '2000-10-21');
 
+delete from reputation;
 insert into reputation(id_target, id_commenter, stance, message) values
 ('vhoyer@live.com', 'test3@email.com','l', 'esse cara é legal pakas'),
 ('vhoyer@live.com', 'test2@email.com','l', 'esse cara é okay...'),
@@ -24,18 +28,22 @@ insert into reputation(id_target, id_commenter, stance, message) values
 ('test3@email.com', 'vhoyer@live.com','l', 'esse cara é legal pakas votou por mim'),
 ('test3@email.com', 'test2@email.com','d', 'esse cara é não legal pakas votou por mim pra baixo D:');
 
+delete from emails;
 insert into emails(email, id_users) values
 ('vhoyer8@gmail', 'vhoyer@live.com');
 
+delete from friends;
 insert into friends(id_request, id_target, date_sent, date_anwser, `status`) values
 ('vhoyer@live.com', 'test3@email.com', '2016-01-10 13:40:00', '2016-01-10 13:40:01', 'a'),
 ('vhoyer@live.com', 'test2@email.com', '2016-01-10 13:40:00', '2016-01-10 13:40:01', 'a'),
 ('test2@email.com', 'test3@email.com', '2016-01-10 13:40:00', null, 'd'),
 ('test5@email.com', 'vhoyer@live.com', '1111-11-11 00:00:00', '1111-11-11 00:00:00', 'a');
 
+delete from tags;
 insert into tags(description) values
 ('I donno y it exists, cuz time pass n memory fade');
 
+delete from groups;
 insert into groups(`name`, tags) values
 ('grupo 1', 1),
 ('tecnicos info', 1),
@@ -44,6 +52,7 @@ insert into groups(`name`, tags) values
 ('tecnicos web', 1),
 ('tecnicos eletro', 1);
 
+delete from rel_groups;
 insert into rel_groups(id_users, id_groups, is_adm) values
 ('vhoyer@live.com', 1, 1),
 ('vhoyer@live.com', 2, 1),
